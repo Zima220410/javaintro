d1f741896a11d03d70d8417e6f94967f4d1e6e74
Zima220410 <47075629+Zima220410@users.noreply.github.com>
2019-02-04 12:10:59 +0200

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,12 @@
+package com.hillel.javaintro.lessons._4;
+
+public class Count {
+    public static void main(String[] args){
+        int count = 1;
+        while (count <= 15){
+            System.out.println("Номер числа:" + count + ",число " + count*7);
+            count++;
+        }
+
+    }
+}
